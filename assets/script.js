a9d77e691ccb1cdd7d0850e92fbdc4f8422b5e08
lithana <https://github.com/settings/emails>
2023-04-17 14:03:22 +0200

    Add dot selected

--- assets/script.js
+++ assets/script.js
@@ -22,22 +22,35 @@ const slides = [
 const arrowLeft = document.querySelector('.arrow_left')
 const arrowRight = document.querySelector('.arrow_right')
 
-const banner = document.querySelector('#banner')
 const dots = document.querySelector('.dots')
+
 const bannerImg = document.querySelector('.banner-img')
-const bannerText = document.querySelector('p')
+bannerImg.src = './assets/images/slideshow/' + slides[0].image
+
+const bannerText = document.querySelector('#banner p')
+bannerText.innerHTML = slides[0].tagLine
+
+const nbrSlides = slides.length
+
+
+
 
 // Event Listener au clic sur flèche
 arrowLeft.addEventListener('click', () => {
-	console.log('All Left') //Test de fonctionnalité
+	console.log('All Left')
 })
-arrowRight.addEventListener('click', () => {
-	console.log('Allright') //Test de fonctionnalité
+arrowRight.addEventListener('click',() => {
+	console.log('Allright')
 })
 
-// Boucle afin de créer un point de navigation pour chaque élément de la const slides
-slides.forEach(img => {
-	dots.innerHTML += '<div class="dot"></div>' // += permet d'ajouter aux éléments existants
-});
+// Boucle afin de créer un point de navigation pour chaque élément de la const slides s'il y en a plus d'un
+for (slide in slides) {
+	const dot = document.createElement('div');
+	dot.className = 'dot';
+	dots.appendChild(dot);
+}
 
-console.log(bannerText)
+dotElement = document.querySelectorAll('.dot')
+dotElement[0].classList.add('dot_selected')
+
+console.log(dotElement)
